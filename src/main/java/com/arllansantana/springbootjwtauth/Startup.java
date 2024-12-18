2e4bba892c9b9f8c2a73e496f5923a25f690745c
Arllan <arllandavid@gmail.com>
2024-12-18 06:52:46 -0300

    Teste de properties 17

--- src/main/java/com/arllansantana/springbootjwtauth/Startup.java
+++ src/main/java/com/arllansantana/springbootjwtauth/Startup.java
@@ -3,11 +3,19 @@ package com.arllansantana.springbootjwtauth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class Startup {
 
 
     public static void main(String[] args) {
+        // Define a porta a partir da variável de ambiente PORT
+        int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "8080"));
+        // Configura a porta no SpringApplication
+        SpringApplication app = new SpringApplication(Startup.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", String.valueOf(port)));
+        app.run(args);
         SpringApplication.run(Startup.class, args);
     }
 
